37e6293597581fbe5d356709dc3906170deed779
Alestln <itworkplace46@gmail.com>
2025-07-12 11:40:46 +0300

    Lesson ended

--- Project_251/Program.cs
+++ Project_251/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello!");
+            Console.WriteLine("My name is \"Alex\".");
 
+            Console.Write("\n\n\n\tI'm 24 yers old.");
         }
     }
 }
